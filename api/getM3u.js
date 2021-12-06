522ee1947dadc031fa77e2ea7faaac61ff421303
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2021-12-06 16:17:59 +0530

    Update getM3u.js

--- api/getM3u.js
+++ api/getM3u.js
@@ -9,7 +9,7 @@ module.exports = async (req, res) => {
         ent: req.query.ent.split('_'),
         tsActive: req.query.sid.split('_')[1] === "D" ? false : true
     };
-    if(uData.tsInActive)
+    if(uData.tsActive)
     {
       let m3uString = await generateM3u(uData);
       res.send(m3uString);
