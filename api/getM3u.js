f663ecedbab91fc47f574bc70f2e4b8dfea413a4
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2022-01-31 17:41:48 +0530

    Update getM3u.js

--- api/getM3u.js
+++ api/getM3u.js
@@ -138,7 +138,6 @@ const getAllChans = async () => {
         m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].detail.dashWidewineLicenseUrl + '&ls_session=';
         m3uStr += jwt.token + '\n';
         m3uStr += chansList[i].detail.dashWidewinePlayUrl + '\n\n';
-        m3uStr = '#EXTINF:-1 tvg-id="154" tvg-name="&TV HD" tvg-logo="https://akamaividz2.zee5.com/image/upload/resources/0-9-tvhd_0/channel_web/1170x658withlogo_378606665.png" tvg-chno="699" group-title="Entertainment",&TV HD\nhttps://rrzee5.000webhostapp.com/z5.php?c=0-9-tvhd_0\n\n'
         
       }
       
