3d8b6941d6e9b5aadaf8b0762666069e738ab6e4
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2022-01-31 17:49:52 +0530

    Update getM3u.js

--- api/getM3u.js
+++ api/getM3u.js
@@ -141,14 +141,13 @@ const getAllChans = async () => {
         
       }
         m3uStr += '#EXTINF:-1 tvg-id="154" tvg-name="&TV HD" tvg-logo="https://akamaividz2.zee5.com/image/upload/resources/0-9-tvhd_0/channel_web/1170x658withlogo_378606665.png" tvg-chno="699" group-title="Entertainment",&TV HD\nhttps://rrzee5.000webhostapp.com/z5.php?c=0-9-tvhd_0\n\n'
-      for (let j = 2; j < 1; j++) {
-        m3uStr += '#EXTINF:-1  tvg-id=' + chansList[j].meta[0].channelId.toString() + '  ';
-        m3uStr += 'tvg-logo=' + chansList[j].meta[0].channelLogo + '   ';
-        m3uStr += 'group-title=' + chansList[j].meta[0].primaryGenre + ',   ' + chansList[j].meta[0].channelName + '\n';
+      for (let i = 0; i < 2; i++) {
+        m3uStr += '#EXTINF:-1  tvg-id=' + chansList[i].meta[0].channelId.toString() + '  ';
+        m3uStr += 'tvg-logo=' + chansList[i].meta[0].channelLogo + '   ';
+        m3uStr += 'group-title=' + chansList[i].meta[0].primaryGenre + ',   ' + chansList[i].meta[0].channelName + '\n';
         m3uStr += '#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha' + '\n';
-        m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[0].detail.dashWidewineLicenseUrl + '&ls_session=';
-        m3uStr += jwt.token + '\n';
-        m3uStr += chansList[j].detail.dashWidewinePlayUrl + '\n\n';  
+        m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].detail.dashWidewineLicenseUrl + '&ls_session=';
+        m3uStr += jwt.token + '\n';  
       }
       
         
