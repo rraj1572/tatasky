7628e4cedaf22bc887314eb7713eea7db16a52b0
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2022-01-31 17:51:21 +0530

    Update getM3u.js

--- api/getM3u.js
+++ api/getM3u.js
@@ -148,6 +148,7 @@ const getAllChans = async () => {
         m3uStr += '#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha' + '\n';
         m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].detail.dashWidewineLicenseUrl + '&ls_session=';
         m3uStr += jwt.token + '\n';  
+        m3uStr += chansList[i].detail.dashWidewinePlayUrl + '\n\n';
       }
       
         
